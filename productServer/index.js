63ae8ff881d3a3a53e7ca4bdbd8dfe5e3ce03249
Albert <albert@weareadaptive.com>
2017-10-04 17:04:31 +0200

    set port 80

--- productServer/index.js
+++ productServer/index.js
@@ -4,7 +4,7 @@ const Hapi = require('hapi');
 const productsList = require('./products.json');
 
 const server = new Hapi.Server();
-server.connection({routes: {cors: true} });
+server.connection({ port: 80, routes: {cors: true} });
 
 const io = require('socket.io')(server.listener);
 
