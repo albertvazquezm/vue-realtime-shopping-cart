84eb9760c44ea2218d94226fc2d69ec1ca421eb2
Albert <albert@weareadaptive.com>
2017-10-04 18:51:06 +0200

    css price flickering

--- src/components/ProductDumb.vue
+++ src/components/ProductDumb.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="product">
+  <div :class="['product', priceChangeClassName]">
     <p class="name">{{product.name}}</p>
     <p class="price">{{product.price}}&euro;</p>
     <div class="clearfix"></div>
@@ -10,14 +10,44 @@
 </template>
 
 <script>
+
+const COLOR_ANIMATION_DURATION_MS = 600;
+
 export default {
   props: ['product'],
+  data() {
+    return {
+      hasRaised: false,
+      hasFallen: false
+    };
+  },
+  computed: {
+    priceChangeClassName() {
+      if(this.hasRaised) {
+        return 'product--price-raised';
+      } else if(this.hasFallen) {
+        return 'product--price-fallen';
+      }
+    }
+  },
   methods: {
     onClickOnAddProductToCart(productId) {
       this.$emit('onAddToCart', productId);
     },
     onClickOnRemoveProductFromCart(productId) {
       this.$emit('onRemoveFromCart', productId);
+    },
+  },
+  watch: {
+    'product.price'(newPrice, oldPrice) {
+      if(newPrice > oldPrice) {
+        this.hasRaised = true;
+        setTimeout(() => this.hasRaised = false, COLOR_ANIMATION_DURATION_MS);
+      }
+      if(newPrice < oldPrice) {
+        this.hasFallen = true;
+        setTimeout(() => this.hasFallen = false, COLOR_ANIMATION_DURATION_MS);
+      }
     }
   }
 }
@@ -26,7 +56,8 @@ export default {
 <style scoped lang="scss">
   .product {
     padding: 10px;
-    background: #eee;
+    background-color: #eee;
+    transition: background-color .3s linear;
     .clearfix {
       float: none;
     }
@@ -43,6 +74,13 @@ export default {
       color: #fff;
       border-radius: 2px;
       float: right;
+    }
+
+    &.product--price-raised {
+      background-color: #CAE8A2;
+    }
+    &.product--price-fallen {
+      background-color: #FF8080;
     }
   }
 </style>
